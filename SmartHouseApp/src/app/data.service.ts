2dcbd7e75bd1e669716e9937837de3fd2936be03
RadmirKashapov <radmir.kashapov.150700@gmail.com>
2020-03-18 20:24:42 +0300

    Added controllers

---
+++ SmartHouseApp/src/app/data.service.ts
@@ -0,0 +1,6 @@
+﻿import { Injectable } from "angular/core"
+import { HttpClient } from "@angular/common/http"
+import { House } from "./house"
+import { Record } from "./record"
+import { Room } from "./room"
+import { Sensor } from "./sensor"
